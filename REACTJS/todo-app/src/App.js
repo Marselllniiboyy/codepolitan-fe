46cb9db8efc0eb0790c44582d2f18b345fc36cd9
Marselllniiboyy <marseljayawijaya@gmail.com>
2025-08-15 16:16:04 +0800

    all done

--- REACTJS/todo-app/src/App.js
+++ REACTJS/todo-app/src/App.js
@@ -1,16 +1,14 @@
-const temp = [
-  { id: 1, todo: "Gym", done: false },
-  { id: 2, todo: "Reading", done: true },
-];
+import { useState } from "react";
 
 function App() {
+  const [items, seItems] = useState([]);
   return (
     <>
       <div className="app">
         <Header />
-        <FormInput />
-        <Taks />
-        <Footer />
+        <FormInput setItems={seItems} items={items} />
+        <Taks items={items} setItems={seItems} />
+        <Footer items={items} />
       </div>
     </>
   );
@@ -24,33 +22,69 @@ function Header() {
   );
 }
 
-function FormInput() {
+function FormInput({ setItems, items }) {
+  const [input, setInput] = useState("");
+
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(e.target.value);
+    if (input.trim() !== "") {
+      setItems([{ id: items.length + 1, todo: input, done: false }, ...items]);
+    }
+    setInput("");
   }
 
   return (
     <div className="form" onSubmit={handleSubmit}>
       <form>
-        <input type="text" placeholder="Enter a task" />
+        <input
+          type="text"
+          placeholder="Enter a task"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
         <button>Submit</button>
       </form>
     </div>
   );
 }
 
-function Taks() {
+function Taks({ items, setItems }) {
+  function handleDoneTask(id) {
+    setItems(
+      items.map((item) =>
+        item.id === id ? { ...item, done: !item.done } : item
+      )
+    );
+    setItems((prev) => {
+      const notDone = prev.filter((item) => !item.done);
+      const done = prev.filter((item) => item.done);
+      return [...notDone, ...done];
+    });
+  }
+
+  function handleDelete(id) {
+    setItems(items.filter((item) => item.id !== id));
+  }
+
   return (
     <div className="task">
       <ul>
-        {temp.map((item) => (
-          <div className="mini-task">
+        {items.map((item) => (
+          <div className="mini-task" key={item.id}>
             <div className="checkbox">
-              <input type="checkbox" />
-              <li key={item.id}>{item.todo}</li>
+              <input
+                type="checkbox"
+                onClick={() => {
+                  handleDoneTask(item.id);
+                }}
+              />
+              <li
+                style={{ textDecoration: item.done ? "line-through" : "none" }}
+              >
+                {item.todo}
+              </li>
             </div>
-            <button>❌</button>
+            <button onClick={() => handleDelete(item.id)}>❌</button>
           </div>
         ))}
       </ul>
@@ -58,11 +92,15 @@ function Taks() {
   );
 }
 
-function Footer() {
+function Footer({ items }) {
+  const doneTask = items.filter((item) => item.done).length;
   return (
     <div className="result">
-      <h2>Taks is 10 , taks Done is 0</h2>
+      <h2>
+        Taks is {items.length} , taks Done is {doneTask}
+      </h2>
     </div>
   );
 }
+
 export default App;
